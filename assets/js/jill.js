80f5b4e25808d26660a01062a7cc3a15fb6e22fb
Wyatt Preul <wpreul@gmail.com>
2014-12-06 14:00:33 -0600

    Clean up dashboard, load boards from API

--- assets/js/jill.js
+++ assets/js/jill.js
@@ -5,10 +5,27 @@
     $.jill.init = function (settings) {
 
         this.settings = settings;
+
+        this.getBoards(function (boards) {
+
+            var count = boards ? boards.length : 0;
+            settings.boardCount.text(count);
+        });
+
+        this.getLogs(function (logs) {
+
+            var count = logs ? logs.length : 0;
+            settings.logCount.text(count);
+        });
     };
 
-    $.jill.getRadios = function (callback) {
+    $.jill.getBoards = function (callback) {
 
-        $.getJSON(this.settings.apiUrl + '/radios', callback);
+        $.getJSON(this.settings.apiUrl + '/boards', callback);
+    };
+
+    $.jill.getLogs = function (callback) {
+
+        $.getJSON(this.settings.apiUrl + '/logs', callback);
     };
 })(jQuery);
